0db9d093153b87456604b0bca50f4c7f74c89f34
phamminhtan2002 <tanphamminh2002@gmail.com>
2023-05-11 15:49:40 -0700

    view height

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ const style = {
 	wrapper: `bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 md:scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80 scroll-smooth`,
 	start: `snap-start`,
 	center: `snap-center`,
-	footer: `sticky bottom-10 w-full cursor-pointer`,
+	footer: `sticky bottom-5 w-full cursor-pointer`,
 	img__container: `flex items-center justify-center`,
 	icon: `h-10 w-10 rounded-full filter opacity-20 hover:text-[#f7ab0a] hover:opacity-100 cursor-pointer`,
 };
